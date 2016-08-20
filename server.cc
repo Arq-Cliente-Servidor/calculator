5cf556de7dfa1c6c4690247d80f6a2b6d1dfc42c
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-08-19 23:37:15 -0500

    moving functions to the class matrix

--- server.cc
+++ server.cc
@@ -9,126 +9,150 @@
 using namespace std;
 using namespace zmqpp;
 
-typedef vector<vector<int>> Matrix;
+template <typename T>
+class Matrix {
+private:
+  vector<vector<T>> elements;
+  int rows, cols;
 
-// convert a string to an matrix
-Matrix createMatrix(string &mat) {
-  Matrix matrix;
-  string line;
+public:
+  Matrix() : rows(0), cols(0) {}
 
-  for (int i = 0; i < mat.size(); i++) {
-    if (mat[i] == '[')
-      continue;
-    if (mat[i] == ']' and line.size()) {
-      vector<int> row;
-      stringstream ss(line);
-      int value;
+  Matrix(vector<vector<T>> el) {
+    this->elements = el;
+    this->rows = elements.size();
+    this->cols = elements[0].size();
+  }
 
-      while (ss >> value) {
-        row.push_back(value);
+  // checks if the matrix has the correct format
+  bool checkFormat(string &mat) {
+    stack<char> brackets;
+    for (int i = 0; i < mat.size(); i++) {
+      if (mat[i] == '[')
+        brackets.push(mat[i]);
+      else if (mat[i] == ']') {
+        if (brackets.empty())
+          return false;
+        brackets.pop();
       }
-
-      matrix.push_back(row);
-      line = "";
-    } else
-      line += mat[i];
-  }
-
-  return matrix;
-}
-
-// checks if it is possible multiply two matrices
-bool checkMult(Matrix &mat1, Matrix &mat2) {
-  int n = mat1[0].size();
-  int m = mat2.size();
-  return n == m;
-}
-
-// checks if the matrix has the correct format
-bool checkMat(string &mat) {
-  stack<char> brackets;
-
-  for (int i = 0; i < mat.size(); i++) {
-    if (mat[i] == '[')
-      brackets.push(mat[i]);
-    else if (mat[i] == ']') {
-      if (brackets.empty()) return false;
-      brackets.pop();
     }
+    return brackets.empty();
   }
 
-  return brackets.empty();
-}
+  // convert a string to an matrix
+  void create(string &mat) {
+    string line;
+    for (int i = 0; i < mat.size(); i++) {
+      if (mat[i] == '[')
+        continue;
+      if (mat[i] == ']' and line.size()) {
+        vector<T> row;
+        stringstream ss(line);
+        T value;
 
-void printMatrix(Matrix &matrix) {
-  for (int i = 0; i < matrix.size(); i++) {
-    for (int j = 0; j < matrix[i].size(); j++) {
-      cout << matrix[i][j] << " ";
+        while (ss >> value) {
+          row.push_back(value);
+        }
+
+        this->elements.push_back(row);
+        line = "";
+      }
+      else line += mat[i];
+    }
+
+    this->rows = elements.size();
+    this->cols = elements[0].size();
+  }
+
+  int getRow() {
+    return this->rows;
+  }
+
+  int getCol() {
+    return this->cols;
+  }
+
+  vector<vector<T>> getElements() {
+    return this->elements;
+  }
+
+  T elementAt(int i, int j) {
+    return this->elements[i][j];
+  }
+
+  // checks if it is possible multiply two matrices
+  bool checkMult(Matrix<T> &mat) {
+    int n = this->getCol();
+    int m = mat.getRow();
+    return n == m;
+  }
+
+  Matrix<T> operator * (Matrix<T> &mat) {
+    int n = this->getRow();
+    int m = this->getCol();
+    int p = mat.getCol();
+    vector<vector<T>> result(n, vector<int>(p));
+
+    for (int i = 0; i < this->getRow(); i++) {
+      for (int j = 0; j < mat.getCol(); j++) {
+        for (int k = 0; k < this->getCol(); k++) {
+          result[i][j] += this->elementAt(i, k) * mat.elementAt(k, j);
+        }
+      }
+    }
+
+    return Matrix<T>(result);
+  }
+
+  string toString() {
+    string text = "[";
+
+    for (int i = 0; i < this->getRow(); i++) {
+      text += "[";
+      for (int j = 0; j < this->getCol(); j++) {
+        if (j)
+          text += " ";
+        text += to_string(this->elementAt(i, j));
+      }
+      text += "]";
+    }
+
+    return text + "]";
+  }
+
+  T determinant() {
+    T pivot = 0;
+    T aux = 0;
+    T ans = 1;
+    vector<vector<T>> result(this->getElements());
+
+    for (int j = 0; j < this->getCol(); j++) {
+      pivot = result[j][j];
+      for (int i = j + 1; i < this->getRow(); i++) {
+        aux = result[i][j];
+        for (int k = 0; k < this->getCol(); k++) {
+          result[i][k] -= result[j][k] * (aux / pivot);
+        }
+      }
+    }
+
+    for (int i = 0; i < rows; i++) {
+      ans *= result[i][i];
+    }
+
+    return ans;
+  }
+
+  void print() {
+    for (int i = 0; i < this->getRow(); i++) {
+      for (int j = 0; j < this->getCol(); j++) {
+        cout << this->elementAt(i, j) << " ";
+      }
+      cout << endl;
     }
     cout << endl;
   }
-  cout << endl;
-}
-
-Matrix multMatrix(Matrix &mat1, Matrix &mat2) {
-  int n = mat1.size();
-  int m = mat2.size();
-  int p = mat2[0].size();
-  Matrix result(n, vector<int>(p));
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < p; j++) {
-      for (int k = 0; k < m; k++) {
-        result[i][j] += mat1[i][k] * mat2[k][j];
-      }
-    }
-  }
-
-  return result;
-}
-
-Matrix detMatrix(Matrix &mat) {
-  int rows = mat.size();
-  int cols = mat[0].size();
-  double pivot = 0.0;
-  double aux = 0.0;
-  vector<vector<double>> result(rows, vector<double>(cols));
-
-  // convert matrix int to matrix double
-  for (int i = 0; i < rows); i++) {
-    for (int j = 0; j < cols; j++) {
-      result[i][j] = mat[i][j];
-    }
-  }
-
-  for (int j = 0; j < cols; j++) {
-    pivot = result[i][i];
-    for (int i = j + 1; i < rows; i++) {
-      aux = result[i][j];
-      for (int k = 0; k < cols; k++) {
-        result[i][k] = result[i][k] - (result[j][k] * pivot / aux);
-      }
-    }
-  }
-
-}
-
-string matToString(Matrix &mat) {
-  string text = "[";
-
-  for (int i = 0; i < mat.size(); i++) {
-    text += "[";
-    for (int j = 0; j < mat[i].size(); j++) {
-      if (j)
-        text += " ";
-      text += to_string(mat[i][j]);
-    }
-    text += "]";
-  }
-
-  return text + "]";
-}
-
+};
 
 int main(int argc, char *argv[]) {
   const string endpoint = "tcp://*:4242";
@@ -155,8 +179,9 @@ int main(int argc, char *argv[]) {
 
     int64_t result = 0;
     int op1 = 0, op2 = 0;
-    Matrix mat1, mat2, mat3;
-    string resultm;
+    Matrix<int> mat1, mat2, mat3;
+    Matrix<double> matDet;
+    string matrixResult;
 
     // Binary operations
     if (op == "add") {
@@ -164,17 +189,20 @@ int main(int argc, char *argv[]) {
       op1 = atoi(ope1.c_str());
       op2 = atoi(ope2.c_str());
       result = op1 + op2;
-    } else if (op == "sub") {
+    }
+    else if (op == "sub") {
       req >> ope2;
       op1 = atoi(ope1.c_str());
       op2 = atoi(ope2.c_str());
       result = op1 - op2;
-    } else if (op == "mult") {
+    }
+    else if (op == "mult") {
       req >> ope2;
       op1 = atoi(ope1.c_str());
       op2 = atoi(ope2.c_str());
       result = op1 * op2;
-    } else if (op == "div") {
+    }
+    else if (op == "div") {
       req >> ope2;
       op1 = atoi(ope1.c_str());
       op2 = atoi(ope2.c_str());
@@ -185,7 +213,8 @@ int main(int argc, char *argv[]) {
     else if (op == "sqrt") {
       op1 = atoi(ope1.c_str());
       result = sqrt(op1);
-    } else if (op == "exp") {
+    }
+    else if (op == "exp") {
       op1 = atoi(ope1.c_str());
       result = exp(op1);
     }
@@ -193,35 +222,39 @@ int main(int argc, char *argv[]) {
     // Matrix operations
     else if (op == "mmult") {
       req >> ope2;
-      if (checkMat(ope1) and checkMat(ope2)) {
-        mat1 = createMatrix(ope1);
-        mat2 = createMatrix(ope2);
+      if (mat1.checkFormat(ope1) and mat1.checkFormat(ope2)) {
+        mat1.create(ope1);
+        mat2.create(ope2);
         // Check if it is posibble multiply mat1 by mat2
-        if (checkMult(mat1, mat2)) {
-          mat3 = multMatrix(mat1, mat2);
-          resultm = matToString(mat3);
-        } else {
-          resultm = "It is not possible to do matrix multiplication";
+        if (mat1.checkMult(mat2)) {
+          mat3 = mat1 * mat2;
+          matrixResult = mat3.toString();
         }
-      } else {
-        resultm = "The matrix was not entered in the correct format";
+        else {
+          matrixResult = "It is not possible to do matrix multiplication";
+        }
+      }
+      else {
+        matrixResult = "The matrix was not entered in the correct format";
       }
     }
 
     else if (op == "mdet") {
-      if (checkMat(ope1)) {
-        mat1 = createMatrix(ope1);
-        resultm = to_string(detMatrix(mat1));
-      } else {
-        resultm = "The matrix was not entered in the correct format";
+      if (matDet.checkFormat(ope1)) {
+        matDet.create(ope1);
+        matrixResult = to_string(matDet.determinant());
+      }
+      else {
+        matrixResult = "The matrix was not entered in the correct format";
       }
     }
 
     if (op == "mmult" or op == "mdet") {
-      rep << resultm;
+      rep << matrixResult;
       s.send(rep);
-      cout << "Sent: " << resultm << endl;
-    } else {
+      cout << "Sent: " << matrixResult << endl;
+    }
+    else {
       rep << result;
       s.send(rep);
       cout << "Sent: " << result << endl;
--- test.cc
+++ test.cc
@@ -2,131 +2,168 @@
 
 using namespace std;
 
-typedef vector<vector<int>> Matrix;
+template <typename T>
+class Matrix {
+private:
+  vector<vector<T>> elements;
+  int rows, cols;
 
-vector<vector<int>> createMatrix(string &mat) {
-  vector<vector<int>> matrix;
-  string line;
+public:
+  Matrix() : rows(0), cols(0) {}
 
-  for (int i = 0; i < mat.size(); i++) {
-    if (mat[i] == '[')
-      continue;
-    if (mat[i] == ']' and line.size()) {
-      vector<int> col;
-      stringstream ss(line);
-      int value;
+  Matrix(vector<vector<T>> el) {
+    this->elements = el;
+    this->rows = elements.size();
+    this->cols = elements[0].size();
+  }
 
-      while (ss >> value) {
-        col.push_back(value);
+  static bool checkFormat(string &mat) {
+    stack<char> brackets;
+    for (int i = 0; i < mat.size(); i++) {
+      if (mat[i] == '[')
+        brackets.push(mat[i]);
+      else if (mat[i] == ']') {
+        if (brackets.empty())
+          return false;
+        brackets.pop();
       }
-
-      matrix.push_back(col);
-      line = "";
-    } else
-      line += mat[i];
-  }
-
-  // cout << "rows: " << ma/trix.size() << " cols: " << matrix[0].size() << endl;
-  return matrix;
-}
-
-bool checkMat(string &mat) {
-  stack<char> brackets;
-
-  for (int i = 0; i < mat.size(); i++) {
-    if (mat[i] == '[')
-      brackets.push(mat[i]);
-    else if (mat[i] == ']') {
-      if (brackets.empty()) return false;
-      brackets.pop();
     }
+    return brackets.empty();
   }
 
-  return brackets.empty();
-}
+  void create(string &mat) {
+    string line;
+    for (int i = 0; i < mat.size(); i++) {
+      if (mat[i] == '[')
+        continue;
+      if (mat[i] == ']' and line.size()) {
+        vector<T> row;
+        stringstream ss(line);
+        T value;
 
-void printMatrix(vector<vector<double>> &matrix) {
-  for (int i = 0; i < matrix.size(); i++) {
-    for (int j = 0; j < matrix[i].size(); j++) {
-      cout << matrix[i][j] << " ";
+        while (ss >> value) {
+          row.push_back(value);
+        }
+
+        this->elements.push_back(row);
+        line = "";
+      } else
+        line += mat[i];
+    }
+
+    this->rows = elements.size();
+    this->cols = elements[0].size();
+  }
+
+  int getRow() {
+    return this->rows;
+  }
+
+  int getCol() {
+    return this->cols;
+  }
+
+  vector<vector<T>> getElements() {
+    return this->elements;
+  }
+
+  T elementAt(int i, int j) {
+    return this->elements[i][j];
+  }
+
+  bool checkMult(Matrix<T> &mat) {
+    int n = this->getCol();
+    int m = mat.getRow();
+    return n == m;
+  }
+
+  Matrix<T> operator * (Matrix<T> &mat) {
+    int n = this->getRow();
+    int m = this->getCol();
+    int p = mat.getCol();
+    vector<vector<T>> result(n, vector<int>(p));
+
+    for (int i = 0; i < this->getRow(); i++) {
+      for (int j = 0; j < mat.getCol(); j++) {
+        for (int k = 0; k < this->getCol(); k++) {
+          result[i][j] += this->elementAt(i, k) * mat.elementAt(k, j);
+        }
+      }
+    }
+
+    return Matrix<T>(result);
+  }
+
+  string toString() {
+    string text = "[";
+
+    for (int i = 0; i < this->getRow(); i++) {
+      text += "[";
+      for (int j = 0; j < this->getCol(); j++) {
+        if (j)
+          text += " ";
+        text += to_string(this->elementAt(i, j));
+      }
+      text += "]";
+    }
+
+    return text + "]";
+  }
+
+  T determinant() {
+    T pivot = 0;
+    T aux = 0;
+    T ans = 1;
+    vector<vector<T>> result(this->getElements());
+
+    for (int j = 0; j < this->getCol(); j++) {
+      pivot = result[j][j];
+      for (int i = j + 1; i < this->getRow(); i++) {
+        aux = result[i][j];
+        for (int k = 0; k < this->getCol(); k++) {
+          result[i][k] -= result[j][k] * (aux / pivot);
+        }
+      }
+    }
+
+    for (int i = 0; i < rows; i++) {
+      ans *= result[i][i];
+    }
+
+    return ans;
+  }
+
+  void print() {
+    for (int i = 0; i < this->getRow(); i++) {
+      for (int j = 0; j < this->getCol(); j++) {
+        cout << this->elementAt(i, j) << " ";
+      }
+      cout << endl;
     }
     cout << endl;
   }
-  cout << endl;
-}
-
-vector<vector<int>> multMatrix(vector<vector<int>> &mat1, vector<vector<int>> &mat2) {
-  int n = mat1[0].size();
-  int m = mat1.size();
-  int p = mat2[0].size();
-  vector<vector<int>> result(m, vector<int>(p));
-
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < p; j++) {
-      for (int k = 0; k < n; k++) {
-        result[i][j] += mat1[i][k] * mat2[k][j];
-      }
-    }
-  }
-
-  return result;
-}
-
-string matToString(vector<vector<int>> &mat) {
-  string text = "[";
-
-  for (int i = 0; i < mat.size(); i++) {
-    text += "[";
-    for (int j = 0; j < mat[i].size(); j++) {
-      if (j)
-        text += " ";
-      text += to_string(mat[i][j]);
-    }
-    text += "]";
-  }
-
-  return text + "]";
-}
-
-double detMatrix(Matrix &mat) {
-  int rows = mat.size();
-  int cols = mat[0].size();
-  double pivot = 0.0;
-  double aux = 0.0;
-  double ans = 1.0;
-  vector<vector<double>> result(rows, vector<double>(cols));
-
-  // convert matrix int to matrix double
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < cols; j++) {
-      result[i][j] = (double)mat[i][j];
-    }
-  }
-
-  for (int j = 0; j < cols; j++) {
-    pivot = result[j][j];
-    for (int i = j + 1; i < rows; i++) {
-      aux = result[i][j];
-      for (int k = 0; k < cols; k++) {
-        result[i][k] -= result[j][k] * (aux / pivot);
-      }
-    }
-  }
-
-  for (int i = 0; i < rows; i++) {
-    ans *= result[i][i];
-  }
-
-  return ans;
-}
+};
 
 int main(int argc, char const *argv[]) {
   // Ejemplo de formato:
   // [[1 2 3][4 5 6][1 2 3]] [[7 8 4][2 3 4][3 2 1]]
+  // [[3 4 -7 6][1 2 -3 4][5 6-7 5][-8 -9 1 2]]
 
   string m1(argv[1]);
-  vector<vector<int>> mat1 = createMatrix(m1);
-  cout << detMatrix(mat1) << endl;
+  // string m2(argv[2]);
+
+  Matrix<int> mat1;
+  // Matrix<int> mat2;
+
+  mat1.create(m1);
+
+  // mat2.create(m2);
+
+  // Matrix<int> mat3 = mat1 * mat2;
+  // mat3.print();
+
+  // vector<vector<int>> mat1 = createMatrix(m1);
+  // cout << detMatrix(mat1) << endl;
   // printMatrix(mat2);
   // cout << checkMat(m1) << endl;
   // string m2(argv[2]);
